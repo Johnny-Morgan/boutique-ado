4cfb10e4b25fe12b7c0c6d99784c1ee9e464dd13
Johnny-Morgan <jwjmorgan@gmail.com>
2021-06-03 12:22:22 +0000

    Add sizes to product model

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, blank=True)
+    friendly_name = models.CharField(max_length=254, blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -19,14 +19,15 @@ class Product(models.Model):
     category = models.ForeignKey(
         'Category', null=True,
         blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, blank=True)
+    sku = models.CharField(max_length=254, blank=True, null=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(
         max_digits=6, decimal_places=2,
         null=True, blank=True)
-    image_url = models.URLField(max_length=1024, blank=True)
+    image_url = models.URLField(max_length=1024, blank=True, null=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
